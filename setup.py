bfecba59828635f6a253eb8a4b58f669341822ec
Raphael Martin <raphael@gonku.de>
2020-11-20 20:07:59 +0100

    Version Bump to 0.1.3-8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
       name='KubunConfector',
-      version='0.1.3-7',
+      version='0.1.3-8',
       description='Kubun Data-Preparation Utility',
       author='Raphael Martin',
       author_email='raphael@kubun.io',
